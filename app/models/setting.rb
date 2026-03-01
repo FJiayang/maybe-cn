d4305f8bce111a89bc81b7a2f2c7f62d26640941
F嘉阳 <fjy8018@qq.com>
2026-03-01 08:30:58 +0800

    feat: add openai_base_url and openai_model_id settings

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -5,6 +5,10 @@ class Setting < RailsSettings::Base
   field :synth_api_key, type: :string, default: ENV["SYNTH_API_KEY"]
   field :openai_access_token, type: :string, default: ENV["OPENAI_ACCESS_TOKEN"]
 
+  # 新增字段 - OpenAI 兼容供应商配置
+  field :openai_base_url, type: :string, default: ENV.fetch("OPENAI_BASE_URL", "https://api.openai.com/v1")
+  field :openai_model_id, type: :string, default: ENV.fetch("OPENAI_MODEL_ID", "gpt-4.1")
+
   field :require_invite_for_signup, type: :boolean, default: false
   field :require_email_confirmation, type: :boolean, default: ENV.fetch("REQUIRE_EMAIL_CONFIRMATION", "true") == "true"
 end
