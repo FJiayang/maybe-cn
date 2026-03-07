34b33a40a6d3379591e9c2fb54bfecbd10420eff
FJiayang <FJiayang@users.noreply.github.com>
2026-03-07 03:45:28 +0000

    fix: 修复 breadcrumbable 测试的 locale 设置
    测试使用 family.locale 而非 I18n.with_locale，因为 switch_locale
    around_action 优先使用 family 的 locale 设置。数据库默认 family.locale
    为 'en'，需要显式设置为 'zh-CN' 进行中文本地化测试。

--- test/controllers/concerns/breadcrumbable_test.rb
+++ test/controllers/concerns/breadcrumbable_test.rb
@@ -9,29 +9,29 @@ class BreadcrumbableTest < ActionDispatch::IntegrationTest
   end
 
   test "dashboard breadcrumbs are localized" do
+    # Set family locale to zh-CN to test localization
+    @user.family.update!(locale: "zh-CN")
     sign_in @user
 
-    I18n.with_locale(:'zh-CN') do
-      get root_path
-      assert_response :success
+    get root_path
+    assert_response :success
 
-      # Check that breadcrumbs use translated values
-      breadcrumbs = @controller.instance_variable_get(:@breadcrumbs)
-      assert_equal "首页", breadcrumbs[0][0]
-      assert_equal "仪表板", breadcrumbs[1][0]
-    end
+    # Check that breadcrumbs use translated values
+    breadcrumbs = @controller.instance_variable_get(:@breadcrumbs)
+    assert_equal "首页", breadcrumbs[0][0]
+    assert_equal "仪表板", breadcrumbs[1][0]
   end
 
   test "accounts breadcrumbs are localized" do
+    # Set family locale to zh-CN to test localization
+    @user.family.update!(locale: "zh-CN")
     sign_in @user
 
-    I18n.with_locale(:'zh-CN') do
-      get accounts_path
-      assert_response :success
+    get accounts_path
+    assert_response :success
 
-      breadcrumbs = @controller.instance_variable_get(:@breadcrumbs)
-      assert_equal "首页", breadcrumbs[0][0]
-      # Second breadcrumb is controller name titleized
-    end
+    breadcrumbs = @controller.instance_variable_get(:@breadcrumbs)
+    assert_equal "首页", breadcrumbs[0][0]
+    # Second breadcrumb is controller name titleized
   end
 end
